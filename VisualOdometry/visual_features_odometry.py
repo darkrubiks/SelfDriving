83cc84b60100335293172ae10af8930cc0fd0f44
darkrubiks <leonardoantunes123@gmail.com>
2021-05-17 22:39:29 -0300

    final code for visual_features_odometry.py, need test

--- VisualOdometry/visual_features_odometry.py
+++ VisualOdometry/visual_features_odometry.py
@@ -1,3 +1,5 @@
+import pickle
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -65,7 +67,7 @@ def filter_matches_distance(match, dist_threshold):
     """
     filtered_match = []
     for m, n in match:
-        if m.distance < dist_threshold*n.distance:
+        if m.distance < dist_threshold * n.distance:
             filtered_match.append(m)
     return filtered_match
 
@@ -108,8 +110,6 @@ def estimate_motion(match, kp1, kp2, k):
                      coordinates of the i-th match in the image coordinate system
 
     """
-    rmat = np.eye(3)
-    tvec = np.zeros((3, 1))
     image1_points = []
     image2_points = []
 
@@ -125,35 +125,31 @@ def estimate_motion(match, kp1, kp2, k):
 
     E, mask = cv2.findEssentialMat(np.array(image1_points), np.array(image2_points), k)
 
-    retval, rmat, tvec, mask = cv2.recoverPose(E, np.array(image1_points), np.array(image2_points), k)
+    retval, rmat, tvec, mask = cv2.recoverPose(E, np.array(image1_points),
+                                               np.array(image2_points), k,
+                                               cv2.RANSAC, 0.999, 1.0)
+
     return rmat, tvec, image1_points, image2_points
 
 
-def estimate_trajectory(estimate_motion, matches, kp_list, k):
+def estimate_trajectory(match, kp1, kp2, k):
     """
     Estimate complete camera trajectory from subsequent image pairs
 
     Arguments:
     estimate_motion -- a function which estimates camera motion from a pair of subsequent image frames
-    matches -- list of matches for each subsequent image pair in the dataset.
-               Each matches[i] is a list of matched features from images i and i + 1
-    des_list -- a list of keypoints for each image in the dataset
+    match -- matches for each subsequent image pair.
+    kp1,kp2 --  keypoints from image
     k -- camera calibration matrix
 
     Returns:
-    trajectory -- a 3xlen numpy array of the camera locations, where len is the lenght of the list of images and
-                  trajectory[:, i] is a 3x1 numpy vector, such as:
+    trajectory -- a numpy array of the camera locations, trajectory[:, i] is a 3x1 numpy vector, such as:
 
                   trajectory[:, i][0] - is X coordinate of the i-th location
                   trajectory[:, i][1] - is Y coordinate of the i-th location
                   trajectory[:, i][2] - is Z coordinate of the i-th location
 
-                  * Consider that the origin of your trajectory cordinate system is located at the camera position
-                  when the first image (the one with index 0) was taken. The first camera location (index = 0) is geven
-                  at the initialization of this function
-
     """
-    trajectory = [np.array([0, 0, 0])]
 
     R = np.diag([1, 1, 1])
     T = np.zeros([3, 1])
@@ -161,36 +157,51 @@ def estimate_trajectory(estimate_motion, matches, kp_list, k):
     RT = np.vstack([RT, np.zeros([1, 4])])
     RT[-1, -1] = 1
 
-    for i in range(len(matches)):
-        match = matches[i]
-        kp1 = kp_list[i]
-        kp2 = kp_list[i + 1]
+    rmat, tvec, image1_points, image2_points = estimate_motion(match, kp1, kp2, k)
+    rt_mtx = np.hstack([rmat, tvec])
+    rt_mtx = np.vstack([rt_mtx, np.zeros([1, 4])])
+    rt_mtx[-1, -1] = 1
 
-        rmat, tvec, image1_points, image2_points = estimate_motion(match, kp1, kp2, k)
-        rt_mtx = np.hstack([rmat, tvec])
-        rt_mtx = np.vstack([rt_mtx, np.zeros([1, 4])])
-        rt_mtx[-1, -1] = 1
+    rt_mtx_inv = np.linalg.inv(rt_mtx)
 
-        rt_mtx_inv = np.linalg.inv(rt_mtx)
+    RT = np.dot(RT, rt_mtx_inv)
+    trajectory = RT[:3, 3]
 
-        RT = np.dot(RT, rt_mtx_inv)
-        new_trajectory = RT[:3, 3]
-        trajectory.append(new_trajectory)
-
-    trajectory = np.array(trajectory).T
     return trajectory
 
 
 if __name__ == '__main__':
 
-    path = ''
-    image = cv2.imread(path)
-    kp, des = extract_features(image)
-    print("Number of features detected in frame {0}: {1}\n".format(path, len(kp)))
-    print("Coordinates of the first keypoint in frame {0}: {1}".format(path, str(kp[0].pt)))
+    with open('CameraCalibration/camera_data.p', 'rb') as fp:
+        k = pickle.load(fp)  # load camera matrix
 
-    visualize_features(image, kp)
+    trajectory = [np.array([0, 0, 0])]
 
+    filter_match = True
 
+    path = ''  # video path
 
+    cap = cv2.VideoCapture(path)
+    ret, previous_frame = cap.read()
+    pre_kp, pre_des = extract_features(previous_frame)  # extract features and descriptor from previous image
+    while cap.isOpened():
+        ret, next_frame = cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            break
+        # extract features and descriptor from next image
+        next_kp, next_des = extract_features(next_frame)
+        # match descriptors
+        match = match_features(pre_des, next_des)
+        # filter match
+        if filter_match:
+            match = filter_matches_distance(match, 0.6)
+        # estimate trajectory
+        new_trajectory = estimate_trajectory(match, pre_kp, next_kp, k)
+        # append to trajectory list
+        trajectory.append(new_trajectory)
+        # re-use the last frame now as the previous frame
+        pre_kp, pre_des = next_kp, next_des
 
+    cap.release()
+    trajectory = np.array(trajectory).T
